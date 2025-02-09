5273454551800787c9a0a6a6666b1a66dc8d80ff
Starl1ght5 <cardertro12@gmail.com>
2025-02-08 21:03:55 -0500

    init repo

---
+++ src/main/java/com/stellargear/royal_airlines/RoyalAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.stellargear.royal_airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalAirlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stellargear/royal_airlines/RoyalAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellargear.royal_airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalAirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
