a456f7ebdbe22ee41183fe5af20057e1ec16e9b6
Starl1ght5 <cardertro12@gmail.com>
2025-03-24 20:58:37 -0500

    init branch

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const router = express()
+const port = 5000;
+const cors = require('cors');
+
+
+router.use(cors());
+router.use(express.json());
+router.post("/start-checkout-session", async(req, res) => {
+
+    const tickets = req.body.items_brought;
+
+    const lineItems = tickets.map( ticket => ({
+        price_data: {
+            currency: "COP",
+            product_data: {
+                name: "Boleto de avion"
+            },
+            unit_amount: parseInt(ticket.totalPrice.amount + "00")
+        },
+        quantity: ticket.ticketCount
+
+    }))
+        
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: "http://localhost:5170/success",
+        cancel_url: "http://localhost:5170/decline"
+    });
+
+    res.json({id: session.id})
+
+})
+
+router.listen(port, () => {
+    console.log(`Servidor backend escuchando en http://localhost:${port}`);
+  });
