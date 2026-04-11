6cf1af1b4ce578d97e86cd24a105c469127a0d7a
Starl1ght5 <cardertro12@gmail.com>
2026-04-10 19:21:39 -0500

    feat: add .env file support

--- src/main/java/com/stellargear/royal_airlines/RoyalAirlinesApplication.java
+++ src/main/java/com/stellargear/royal_airlines/RoyalAirlinesApplication.java
@@ -1,13 +1,17 @@
 package com.stellargear.royal_airlines;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class RoyalAirlinesApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach((DotenvEntry e) -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(RoyalAirlinesApplication.class, args);
 	}
-
 }
